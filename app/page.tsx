3bcb9305c7902390af374189f799d78d9cfb8817
Neuron-NB 178 <novry.mahdevika@neuron.co.id>
2026-01-18 23:38:06 +0700

    feat: something great

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,101 @@
-import Image from "next/image";
+"use client";
+
+import gsap from 'gsap';
+import { useRef } from "react";
+import { useGSAP } from '@gsap/react';
+import { SplitText} from 'gsap/all'
+import { GSDevTools } from 'gsap/GSDevTools'
+
+gsap.registerPlugin(SplitText);
+gsap.registerPlugin(GSDevTools);
 
 export default function Home() {
+  const containerRef = useRef<HTMLDivElement>(null);
+  
+  useGSAP(
+    () => {
+      const split = new SplitText(".title", {
+        type: "words, chars",
+        wordsClass: "title-word++",
+        charsClass: "title-char++",
+        mask: "chars"
+      });
+      
+      const tl = gsap.timeline()
+      
+      tl.from(".title-word1 .title-char", {
+        y: "100%",
+        duration: 0.5,
+        stagger: 0.07,
+        ease: "circ.out",
+      })
+      
+      tl.from(".title-word2 .title-char", {
+        x: "-100%",
+        duration: 0.5,
+        stagger: 0.07,
+        ease: "circ.out",
+      }, "-=0.5s")
+      
+      tl.from(".tl-start", {
+        height: 0,
+        duration: 0.5,
+        ease: "circ.out",
+      }, "<")
+      
+      tl.from(
+        ".tl-main",
+        {
+          width: 0,
+          duration: 0.8,
+          ease: "circ.out",
+        },
+        "<+0.2s"
+      );
+      
+      tl.from(
+        ".title-word3 .title-char",
+        {
+          y: "-100%",
+          duration: 0.3,
+          stagger: 0.07,
+          ease: "circ.out",
+        },
+        "-=0.5s"
+      );
+      
+      tl.to(
+        ".tl-dot",
+        {
+          opacity: 1,
+          duration: 0.01,
+          repeat: 6,
+          yoyo: true,
+          repeatDelay: 0.05,
+          ease: "circ.out",
+        },
+        1
+      );
+
+      new GSDevTools({ animation: tl });
+    },
+    {
+      scope: containerRef,
+    }
+  );
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="bg-blue-300 text-black">
+      <div
+        ref={containerRef}
+        className="bg-blue-300 text-black flex h-screen items-end justify-left overflow-hidden"
+      >
+        <h1 className="title font-black text-[min(20rem,30vw)] leading-none pb-[0.1em] text-left">
+          Coming
+          <br />
+          Soon!
+        </h1>
+      </div>
     </div>
   );
 }
